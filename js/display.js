f6e11977f3d789d08da27440045aa678de61ff04
Bello <gbahdeybohbello@gmail.com>
2020-03-18 18:41:46 +0100

    Added necessary assets for the project

---
+++ js/display.js
@@ -0,0 +1,32 @@
+/**
+* Creche App - "web link"
+
+
+* @param {Object} DisplayStuffs - class to dsiplay stuffs in the DOM
+* @property {String} flex
+* @property {String} block
+* @property {String} none
+* @param {method} displayStuff - set the stuffs display to `block`
+* @param {method} displayFlexStuff - set's the stuffs display to `flex`
+* @param {method} hideStuff - set's the stuffs display to `none`
+* @param {mixed} stuff - the DOM Object to be displayed
+*/
+class DisplayStuffs {
+    constructor() {
+        this.flex = "flex";
+        this.block = "block";
+        this.none = "none";
+    }
+    displayStuff(stuff) {
+        stuff.style.display = this.block;
+        document.body.style.overflow = 'hidden'; //Disable scrolling on the main body
+    }
+    displayFlexStuff(stuff) {
+        stuff.style.display = this.flex;
+        document.body.style.overflow = 'hidden'; //Disable scrolling on the main body
+    }
+    hideStuff(stuff) {
+        stuff.style.display = this.none;
+        // document.body.style.overflow = 'scroll'; //Disable scrolling on the main body
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', () => {
+	// When the DOM has loaded.
+})
