595d6d1faf2004c4cd5bcaca19cc315dcf56a5bc
Bello <gbahdeybohbello@gmail.com>
2020-03-18 22:15:48 +0100

    Wrote the side Menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
 	// When the DOM has loaded.
+	// Highligh the selected side menu option 
+
+	let sideMenuText = document.querySelectorAll('.sideMenuOptions');
+	sideMenuText = Array.from(sideMenuText);
+
+	sideMenuText.forEach(menu => {
+		menu.addEventListener('click', () => {
+			// Make all menus inactive initially
+			sideMenuText.forEach(innerMenu => {
+				innerMenu.classList.remove('active');
+				// Change the icon to the default icon
+				let src = innerMenu.querySelector('img');
+				let newSrc = src.src.split('/');
+				newSrc.pop();
+				newSrc.push('mail_grey.svg');
+				newSrc = newSrc.join('/');
+				src.src = newSrc;
+			})
+			// Make the selected menu Active
+			menu.classList.add('active');
+			// Change the icon to the default icon
+			let src = menu.querySelector('img');
+			let newSrc = src.src.split('/');
+			newSrc.pop();
+			newSrc.push('mail.svg');
+			newSrc = newSrc.join('/');
+			src.src = newSrc;
+		});
+	})
 })
