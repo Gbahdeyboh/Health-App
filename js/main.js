a93eaf9080910f46748c44131f51bbd4cfced1bf
Bello <gbahdeybohbello@gmail.com>
2020-03-18 23:23:56 +0100

    Made the side menu Interactive

--- js/main.js
+++ js/main.js
@@ -2,31 +2,44 @@ document.addEventListener('DOMContentLoaded', () => {
 	// When the DOM has loaded.
 	// Highligh the selected side menu option 
 
-	let sideMenuText = document.querySelectorAll('.sideMenuOptions');
-	sideMenuText = Array.from(sideMenuText);
-
-	sideMenuText.forEach(menu => {
-		menu.addEventListener('click', () => {
-			// Make all menus inactive initially
-			sideMenuText.forEach(innerMenu => {
-				innerMenu.classList.remove('active');
-				// Change the icon to the default icon
-				let src = innerMenu.querySelector('img');
-				let newSrc = src.src.split('/');
-				newSrc.pop();
-				newSrc.push('mail_grey.svg');
-				newSrc = newSrc.join('/');
-				src.src = newSrc;
-			})
-			// Make the selected menu Active
-			menu.classList.add('active');
-			// Change the icon to the default icon
-			let src = menu.querySelector('img');
-			let newSrc = src.src.split('/');
-			newSrc.pop();
-			newSrc.push('mail.svg');
-			newSrc = newSrc.join('/');
-			src.src = newSrc;
-		});
-	})
 })
+function selectPings(){
+	let sideMenuPing = document.querySelector('#sideMenuPing');
+	let sideMenuStudent = document.querySelector('#sideMenuStudent');
+	// Make the ping menu active
+	sideMenuPing.classList.add('active');
+	sideMenuStudent.classList.remove('active');
+	// Change the icon to the default icon
+	let src = sideMenuStudent.querySelector('img');
+	let newSrc = src.src.split('/');
+	let name = newSrc.pop();
+	newSrc.push('user_grey.svg');
+	newSrc = newSrc.join('/');
+	src.src = newSrc; 
+
+	let pingSrc = sideMenuPing.querySelector('img').src.split('/');
+	let pingName = pingSrc.pop();
+	pingSrc.push('mail.svg');
+	pingSrc = pingSrc.join('/');
+	sideMenuPing.querySelector('img').src = pingSrc;
+}
+
+function selectStudent(){
+	let sideMenuPing = document.querySelector('#sideMenuPing');
+	let sideMenuStudent = document.querySelector('#sideMenuStudent');
+	// Make the ping menu active
+	sideMenuPing.classList.remove('active');
+	sideMenuStudent.classList.add('active');
+	// Change the icon to the default icon
+	let src = sideMenuStudent.querySelector('img');
+	let newSrc = src.src.split('/');
+	let name = newSrc.pop();
+	newSrc.push('user.svg');
+	newSrc = newSrc.join('/');
+	src.src = newSrc;
+	let pingSrc = sideMenuPing.querySelector('img').src.split('/');
+	let pingName = pingSrc.pop();
+	pingSrc.push('mail_grey.svg');
+	pingSrc = pingSrc.join('/');
+	sideMenuPing.querySelector('img').src = pingSrc;
+}
