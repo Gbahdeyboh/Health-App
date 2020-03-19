9fa5d7ea24d3283011bce8f20c6c8ec89cb489da
Bello <gbahdeybohbello@gmail.com>
2020-03-19 15:54:55 +0100

    Implemented the selection of page to display

--- js/main.js
+++ js/main.js
@@ -16,18 +16,23 @@ function selectPings(){
 	newSrc.push('user_grey.svg');
 	newSrc = newSrc.join('/');
 	src.src = newSrc; 
-
+	// Make the student Icon inactive
 	let pingSrc = sideMenuPing.querySelector('img').src.split('/');
 	let pingName = pingSrc.pop();
 	pingSrc.push('mail.svg');
 	pingSrc = pingSrc.join('/');
 	sideMenuPing.querySelector('img').src = pingSrc;
+	// display the ping section and hide the student section
+	let studentsSection = document.querySelector('#studentsSection');
+	let pingsSection = document.querySelector('#pingsSection');
+	pingsSection.style.display = "block";
+	studentsSection.style.display = "none";
 }
 
 function selectStudent(){
 	let sideMenuPing = document.querySelector('#sideMenuPing');
 	let sideMenuStudent = document.querySelector('#sideMenuStudent');
-	// Make the ping menu active
+	// Make the student menu active
 	sideMenuPing.classList.remove('active');
 	sideMenuStudent.classList.add('active');
 	// Change the icon to the default icon
@@ -37,9 +42,15 @@ function selectStudent(){
 	newSrc.push('user.svg');
 	newSrc = newSrc.join('/');
 	src.src = newSrc;
+	// Make the ping menu inactive
 	let pingSrc = sideMenuPing.querySelector('img').src.split('/');
 	let pingName = pingSrc.pop();
 	pingSrc.push('mail_grey.svg');
 	pingSrc = pingSrc.join('/');
 	sideMenuPing.querySelector('img').src = pingSrc;
+	// display the student section and hide the ping section
+	let studentsSection = document.querySelector('#studentsSection');
+	let pingsSection = document.querySelector('#pingsSection');
+	pingsSection.style.display = "none";
+	studentsSection.style.display = "block";
 }
